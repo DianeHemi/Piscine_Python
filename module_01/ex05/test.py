2b218e9f415828fece67feaae076449be5d2ab7f
DianeHemi <dchampda@student.42.fr>
2022-03-31 14:58:35 +0200

    Working on ex05

--- module_01/ex05/test.py
+++ module_01/ex05/test.py
@@ -2,14 +2,15 @@ from the_bank import Account, Bank
 
 bank = Bank()
 a1 = Account("Diane")
-#a1.transfer(1000)
+a1.transfer(1000)
 a2 = Account("Loic")
-#a2.transfer(500)
+a2.transfer(500)
 a3 = Account("Jim")
-#a3.transfer(200)
+a3.transfer(200)
 
 bank.add(a1)
 bank.add(a2)
 bank.add(a3)
 
-bank.transfer(a1, a2, 20)
+bank.transfer(a1, a2, 200)
+bank.fix_account(a1)
--- module_01/ex05/the_bank.py
+++ module_01/ex05/the_bank.py
@@ -1,5 +1,6 @@
 class Account(object):
     ID_COUNT = 1
+    value = 0
     
     def __init__(self, name, **kwargs):
         self.id = self.ID_COUNT
@@ -28,23 +29,21 @@ class Bank:
         @return True if success, False if an error occured
         """
         
-        if dir(dest) != dir(origin):
-            raise Exception("Sender or receiver is not an account")
+        if dir(dest) != dir(origin) or amount < 0:
+            return False
         
         if origin not in self.account or dest not in self.account:
-            raise Exception("Sender or receiver is not a registered account")
-        
-        if amount < 0:
-            raise Exception("Transfer amount cannot be negative")
+            return False
 
         sender = next(x for x in self.account if origin.id == x.id or origin.name == x.name)
         receiver = next(x for x in self.account if dest.id == x.id or dest.name == x.name)
-        print(sender.name, "to", receiver.name)
-        
-
-        
-
-        
+        if sender.value < amount:
+            return False
+        else:
+            sender.transfer(-amount)
+            receiver.transfer(amount)
+        return True     
+   
         
         
     def fix_account(self, account):
@@ -54,3 +53,26 @@ class Bank:
         @return True if success, False if an error occured
         """
         
+        acc = next(x for x in self.account if account.id == x.id or account.name == x.name)
+        acc_data = dir(account)
+        
+        if "name" not in acc_data:
+            acc.name = ""
+        elif "id" not in acc_data:
+            acc.id = acc.ID_COUNT
+        elif "value" not in acc_data:
+            acc.value = 0
+        
+        for element in acc_data:
+            if element.startswith("__b") or element.startswith("b"):
+                del element
+        
+        if len([x for x in acc_data if x.startswith('zip')]) == 0:
+            acc.zip = ""
+        if len([x for x in acc_data if x.startswith('addr')]) == 0:
+            acc.addr = ""
+        
+        if len(acc_data) % 2 != 1:
+            return False
+        
+        return True
