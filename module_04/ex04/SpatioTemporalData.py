a6e664df672f87f3439a07f7e101bb9f8d79f7cf
DianeHemi <dchampda@student.42.fr>
2022-04-08 16:50:28 +0200

    Correction on ex04

--- module_04/ex04/SpatioTemporalData.py
+++ module_04/ex04/SpatioTemporalData.py
@@ -19,14 +19,6 @@ class SpatioTemporalData:
         data = self.df[self.df['Year'] == date]
         if len(data) == 0:
             return None
-        
-        index = data[data['Event'].str.contains("Olympic")]
-        if len(index) == 0:
-            index = data[data['Team'].str.contains("Olympic")]
-        if len(index) == 0:
-            index = data[data['Games'].str.contains("Olympic")]
-        if len(index) == 0:
-            return None
 
         index = index.drop_duplicates(subset=['City'])
         return(index['City'].values)
